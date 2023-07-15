5cd4d1f046fec29919bbb6eb96a64da762a8d485
Ahnyspecter <mspecter15@gmail.com>
2023-07-15 15:30:36 -0700

    models task done

---
+++ models/state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represents a state.
+    Attributes:
+        name (str): Name of the state.
+    """
+
+    name = ""#!/usr/bin/python3
+"""Defines the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represents a state.
+    Attributes:
+        name (str): Name of the state.
+    """
+
+    name = ""
